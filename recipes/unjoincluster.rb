37db7c30ba5ce9244c33bc427a3d6a2dab477ad8
mspk-sebastian <sebastian@mediaspike.net>
2016-05-11 20:19:38 -0400

    Test

--- recipes/unjoincluster.rb
+++ recipes/unjoincluster.rb
@@ -4,6 +4,10 @@ key_name 	 = node[:zendserver][:apikeyname]
 key_secret   = node[:zendserver][:apikeysecret]
 
 node_id = get_node_id_by_name(node['hostname'], key_name, key_secret)
+
+puts node
+puts "\n"
+
 unjoin_command = "#{node[:zendserver][:zsmanage]} cluster-remove-server #{node_id} -N #{key_name} -K #{key_secret} -U http://#{node[:hostname]}:10081/ZendServer/ -s"
 
 log "Removing server node #{node['hostname']} from cluster - Command: #{unjoin_command}" if is_node_joined(key_name, key_secret)
